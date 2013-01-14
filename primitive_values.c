7338062f2b354ef78444bdde30ed71c0907f84dd
Mariz Melo <mm@xchema.com>
2013-01-13 20:00:40 -0800

    primitive_values.c

---
+++ primitive_values.c
@@ -0,0 +1,11 @@
+/*
+    This program prints the value of all integer variable types available in C
+*/
+#include <stdio.h>
+
+int main()
+{
+printf("size of a short is %lu\n", sizeof(short));
+printf("size of a int is %lu\n", sizeof(int));
+printf("size of a long is %lu\n", sizeof(long));
+}
